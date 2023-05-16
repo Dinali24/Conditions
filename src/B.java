72a6e06e34ae8c72f14f7c3fe205d0f0f8cc6a5b
Dinali24 <devonshehara0824@gmail.com>
2023-05-16 08:30:20 +0530

    condition

---
+++ src/B.java
@@ -0,0 +1,15 @@
+public class B {
+    public static void main(String[] args) {
+        String a = "Dinali";
+
+        if (a == "Teddy"){ //(a.equals("Teddy"))
+            System.out.println("Teddy");
+        }
+        else if (a == "Dinali") {//(a.equals("Bunny"))
+            System.out.println("Dinali");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
